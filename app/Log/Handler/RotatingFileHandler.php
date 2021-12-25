e97efb6ab81f73aa3fb0baf2d4a0a9d18c6e47d1
hcg <532508307@qq.com>
2021-12-25 18:44:19 +0800

    重写logger日志处理器，为以后扩展发送日志数据到第三方日志服务平台做基础

--- app/Log/Handler/RotatingFileHandler.php
+++ app/Log/Handler/RotatingFileHandler.php
@@ -6,20 +6,34 @@ namespace App\Log\Handler;
 
 use Monolog\Handler\RotatingFileHandler as MonologRotatingFileHandler;
 
-/*
+/**
  * 日志处理器
- * */
+ *
+ * 这个日志处理器的主要目的，是为了在Monolog\Handler\RotatingFileHandler处理日志之后，
+ * 可扩展自己的日志处理逻辑而设计的。
+ *
+ * @author Hcg <532508307@qq.com>
+ */
 class RotatingFileHandler extends MonologRotatingFileHandler
 {
     /**
-     * 重写write方法
-     * 用于未来在写入日志的同时发送日志数据到第三方日志服务平台
-     * {@inheritDoc}
+     * 重写父类write方法
+     * @author Hcg <532508307@qq.com>
      */
     protected function write(array $record): void
     {
-        // todo 说明：$record变量中有我们需要得日志信息，在这里扩展发送日志数据到第三方日志服务平台即可
-
         parent::write($record);
+        $this->post($record);
+    }
+
+    /**
+     * 发送日志数据到第三方日志服务平台
+     *
+     * @param array $record 日志信息
+     *
+     * @author Hcg <532508307@qq.com>
+     */
+    protected function post(array $record){
+        // todo 在这里扩展发送日志数据到第三方日志服务平台
     }
 }
