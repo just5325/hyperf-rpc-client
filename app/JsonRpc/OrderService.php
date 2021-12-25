1388560ea1f0e8fe7a323ae5f601ba4ef5dbfcc4
hcg <532508307@qq.com>
2021-12-25 15:10:10 +0800

    “OrderService”服务调用“userService”服务

--- app/JsonRpc/OrderService.php
+++ app/JsonRpc/OrderService.php
@@ -4,6 +4,7 @@ namespace App\JsonRpc;
 
 use Hyperf\RpcServer\Annotation\RpcService;
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Di\Annotation\Inject;
 
 /**
  * 注意，如希望通过服务中心来管理服务，需在注解内增加 publishTo 属性
@@ -11,8 +12,17 @@ use Hyperf\Contract\ConfigInterface;
  */
 class OrderService implements OrderServiceInterface
 {
+
+    /**
+     * 通过 `@Inject` 注解注入由 `@var` 注解声明的属性类型对象
+     *
+     * @Inject(lazy=true)
+     * @var UserServiceInterface
+     */
+    public $userService;
+
     public function getOrderInfo(int $id): array
     {
-        return ['id' => $id, 'name' => '订单详情'];
+        return ['id' => $id, 'name' => '订单详情', 'user_info' => $this->userService->getUserInfo($id)];
     }
 }
