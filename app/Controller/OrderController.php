ca58857d0ec0d4ae7f08b43d34606e889f637279
hcg <532508307@qq.com>
2021-12-25 18:03:57 +0800

    配置logger日志格式为 json, 并且日志文件按日期分割文件，方便投递到第三方日志服务

--- app/Controller/OrderController.php
+++ app/Controller/OrderController.php
@@ -10,7 +10,9 @@ use App\JsonRpc\ResponseServiceInterface;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Contract\ResponseInterface;
+use Hyperf\Logger\LoggerFactory;
 use Psr\Http\Message\ResponseInterface as Psr7ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 class OrderController
 {
@@ -31,13 +33,28 @@ class OrderController
      */
     public $responseService;
 
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(LoggerFactory $loggerFactory)
+    {
+        // 第一个参数对应日志的 name, 第二个参数对应 config/autoload/logger.php 内的 key
+        $this->logger = $loggerFactory->get('log', 'default');
+    }
+
     // 获取用户信息
     public function getOrderInfo(RequestInterface $request)
     {
-        throw new \App\Exception\ApiException('手动抛出接口异常');
+        $this->logger->info("测试日志信息", ['a'=>1,'b'=>2]);
+        $this->logger->error("测试错误信息", ['a'=>10,'b'=>20]);
+        $this->logger->debug("测试debug信息", ['a'=>100,'b'=>200]);
+//        throw new \App\Exception\ApiException('手动抛出接口异常');
         // 从请求中获得 id 参数
         $id = $request->input('id', 1);
         $ret = $this->orderService->getOrderInfo((int)$id);
         return $this->responseService->ApiSuccess($ret);
     }
+
 }
--- config/autoload/logger.php
+++ config/autoload/logger.php
@@ -12,18 +12,17 @@ declare(strict_types=1);
 return [
     'default' => [
         'handler' => [
-            'class' => Monolog\Handler\StreamHandler::class,
+            'class' => Monolog\Handler\RotatingFileHandler::class,
             'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => Monolog\Logger::DEBUG,
+                'filename' => BASE_PATH . '/runtime/logs/hyperf-json.log',
+                'level' => Monolog\Logger::INFO,
             ],
-        ],
-        'formatter' => [
-            'class' => Monolog\Formatter\LineFormatter::class,
-            'constructor' => [
-                'format' => null,
-                'dateFormat' => 'Y-m-d H:i:s',
-                'allowInlineLineBreaks' => true,
+            'formatter' => [
+                'class' => Monolog\Formatter\JsonFormatter::class,
+                'constructor' => [
+                    'batchMode' => Monolog\Formatter\JsonFormatter::BATCH_MODE_JSON,
+                    'appendNewline' => true,
+                ],
             ],
         ],
     ],
